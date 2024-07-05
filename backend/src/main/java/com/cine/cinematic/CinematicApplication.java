68eb45a502ea16fcb2d895650d0b4ab9ba965f70
devEmersonc <dev.emersonc@gmail.com>
2024-07-05 01:35:22 -0400

    first-commit

---
+++ backend/src/main/java/com/cine/cinematic/CinematicApplication.java
@@ -0,0 +1,13 @@
+package com.cine.cinematic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cine/cinematic/CinematicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.cinematic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
